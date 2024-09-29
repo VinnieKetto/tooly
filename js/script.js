388daf74e08fe13e728940d93515da24784222db
VinnieKetto <dark.codx.lowly@gmail.com>
2024-09-29 17:32:05 +0500

    0.0.9
    The ability to get information about the application has been added. There has also been a major refactoring of the scripts.

--- js/script.js
+++ js/script.js
@@ -5,23 +5,34 @@ document.addEventListener("DOMContentLoaded", () => {
 	const mainImages = document.querySelectorAll(".main__item img");
 	const menu = document.querySelector(".header__menu");
 	const info = document.querySelector(".footer__info");
+	const infoBlock = document.querySelector(".footer__info-block");
 
-	icon.onclick = () => {
-		document.body.classList.toggle("light");
-		if (document.body.classList.contains("light")) {
+	if (icon && mainImages.length > 0 && menu && info) {
+		const setBrightness = (brightnessValue) => {
 			mainImages.forEach(image => {
-				image.style.filter = "brightness(6.55%)";
+				image.style.filter = brightnessValue;
 			});
-			menu.style.filter = info.style.filter = "brightness(6.55%)";
-			icon.src = "img/icons/moon.svg";
-		} 
-		else {
-			mainImages.forEach(image => {
-				image.style.filter = "none"; 
+			menu.style.filter = info.style.filter = brightnessValue;
+		};
+
+		icon.addEventListener("click", () => {
+			document.body.classList.toggle("light");
+			if (document.body.classList.contains("light")) {
+				setBrightness("brightness(6.55%)");
+				icon.src = "img/icons/moon.svg";
+			} 
+			else {
+				setBrightness("none");
+				icon.src = "img/icons/sun.svg";
+			}
+		});
+		
+		if (infoBlock) {
+			info.addEventListener("click", () => {
+				const isHidden = infoBlock.style.transform === "scale(0)";
+				infoBlock.style.transform = isHidden ? "scale(1)" : "scale(0)";
 			});
-			menu.style.filter = info.style.filter = "none";
-			icon.src = "img/icons/sun.svg";
 		}
-	};
+	}
 });
 
